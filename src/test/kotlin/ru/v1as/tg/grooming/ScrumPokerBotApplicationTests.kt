569a4e18fe0076a86f9f37c7c17602b0515d5206
v1as <ivlasishen@gmail.com>
2023-09-27 18:06:06 +0300

    only start command

--- src/test/kotlin/ru/v1as/tg/grooming/ScrumPokerBotApplicationTests.kt
+++ src/test/kotlin/ru/v1as/tg/grooming/ScrumPokerBotApplicationTests.kt
@@ -2,8 +2,10 @@ package ru.v1as.tg.grooming
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ScrumPokerBotApplicationTests {
 
     @Test
